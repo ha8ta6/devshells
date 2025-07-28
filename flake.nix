60a43abde602b9fa65fc248130a4ffbd086589d9
Hayato Tamura <213896824+ha8ta6@users.noreply.github.com>
2025-07-28 12:41:09 +0900

    🔧 (devenv) Add TOML tooling to pre-commit hook (#23)
    * 🔧 (devenv) Add TOML formatter to pre-commit hook (#20)
    
    * 🔧 (devenv) Add TOML static analysis tools to pre-commit hook (#20)
    
    ---
    
    close #20

--- flake.nix
+++ flake.nix
@@ -43,6 +43,17 @@
               after = [ "nixfmt-tree" ];
             };
 
+            taplo = {
+              enable = true;
+              description = "Format the TOML files.";
+            };
+
+            check-toml = {
+              enable = true;
+              description = "Detects anti-patterns in TOML files.";
+              after = [ "taplo" ];
+            };
+
             prettier = {
               enable = true;
               description = "Format the YAML and Markdown files.";
