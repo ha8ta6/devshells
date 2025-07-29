94706e8a4934571f7efe9faaed4fe2c8ea859742
ha8ta6 <213896824+ha8ta6@users.noreply.github.com>
2025-07-29 13:28:46 +0000

    ⏪️ (devenv) Revert "🔧 (devenv) Add TOML tooling to pre-commit hook (#23)" (#24)
    This reverts commit 60a43ab.

--- flake.nix
+++ flake.nix
@@ -43,17 +43,6 @@
               after = [ "nixfmt-tree" ];
             };
 
-            taplo = {
-              enable = true;
-              description = "Format the TOML files.";
-            };
-
-            check-toml = {
-              enable = true;
-              description = "Detects anti-patterns in TOML files.";
-              after = [ "taplo" ];
-            };
-
             prettier = {
               enable = true;
               description = "Format the YAML and Markdown files.";
